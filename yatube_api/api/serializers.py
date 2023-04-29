1ba412b14aa5f6f46e41f645a6c88823c50ba5a9
Daniil Petrov <octrow@yandex.ru>
2023-04-29 14:41:35 +0600

    v.1.2

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,7 +1,5 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
-from rest_framework.validators import UniqueTogetherValidator
-
 from posts.models import Comment, Follow, Group, Post, User
 
 
